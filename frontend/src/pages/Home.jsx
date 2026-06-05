e42e07e2d5885940c339c98ba6f5edb9152c81cc
Vighnesh <vghnshpjr@gmail.com>
2026-06-05 15:55:13 +0530

    Home page created

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+        <div>
+            <h1>Scholarship Information Portal</h1>
+            <p>Find scholarships easily.</p>
+        </div>
+    );
+}
+export default Home;
