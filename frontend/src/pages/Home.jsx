fc97a282d8278987320df8fe66b83defb0eaf57d
Vighnesh <vghnshpjr@gmail.com>
2026-06-05 16:03:07 +0530

    Added Homepage layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,19 @@ function Home(){
     return (
         <div>
             <h1>Scholarship Information Portal</h1>
+            <input type="text" placeholder="Search Sclrships.."/>
+            <br/><br/>
+            <button>Search</button>
+            <h2>featured Scholarships</h2>
+            <div>
+                <h3>National Scholarships</h3>
+                <p>Amount :Rupee 50000</p>
+                <h3>State Scholarships</h3>
+                <p>Amount :Rupee 25000</p>
+                <div/>
             <p>Find scholarships easily.</p>
         </div>
+    </div>
     );
 }
 export default Home;
