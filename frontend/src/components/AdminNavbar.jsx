072d36fbcc5ac6b7356033f4a01dcda4050c44c8
Vighnesh <vghnshpjr@gmail.com>
2026-06-17 14:43:39 +0530

    Updated

---
+++ frontend/src/components/AdminNavbar.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import "./AdminNavbar.css";
+
+function AdminNavbar() {
+
+    const navigate = useNavigate();
+
+    return (
+        <nav className="admin-navbar">
+            <h2>Scholarship Portal - Admin</h2>
+
+            <button
+                onClick={() => navigate("/admin")}
+                className="admin-home-btn"
+            >
+                Admin Dashboard
+            </button>
+        </nav>
+    );
+}
+
+export default AdminNavbar;
