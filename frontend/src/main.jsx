cd6b89c2993a06467a66acbeb0dadf43c558203b
Vighnesh <vghnshpjr@gmail.com>
2026-07-13 18:25:46 +0530

    Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,17 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <ToastContainer />
+    <ToastContainer 
+    
+      position="top-right"
+  autoClose={3000}
+  hideProgressBar={false}
+  newestOnTop
+  closeOnClick
+  pauseOnHover
+  draggable
+  theme="colored"
+
+    />
   </StrictMode>,
 )
