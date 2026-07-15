4af5fbfca81bc652731a8e3c3ba7e4e599ff9d0f
Vighnesh <vghnshpjr@gmail.com>
2026-07-15 16:01:20 +0530

    Updates

--- frontend/src/features/admin/pages/Admin.jsx
+++
@@ -1,8 +0,0 @@
-import Dashboard from "../components/Dashboard";
-// import { Link, Outlet } from "react-router-dom";
-// import "../components/MS.css";
-function Admin() {
-  return <Dashboard />;
-}
-
-export default Admin;
