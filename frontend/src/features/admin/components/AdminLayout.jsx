a6ed62b065de805e52fbe20e5ea07aef45ba34b0
Vighnesh <vghnshpjr@gmail.com>
2026-07-27 12:40:06 +0530

    Update AdminLayout

--- frontend/src/features/admin/components/AdminLayout.jsx
+++ frontend/src/features/admin/components/AdminLayout.jsx
@@ -1,17 +1,39 @@
-import { Outlet, replace } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import "./Sidebar.css";
 import { LogOut } from "lucide-react";
-const NAV_ITEMS = [
-  { key: "Admindashboard", label: "Dashboard", path: "/admin" },
-  { key: "view", label: "View Scholarships-Available", path: "/admin/view" },
-  { key: "manage", label: "Create-Scholarships", path: "/admin/manage" },
-  { key: "students", label: "View Students-DATA", path: "/admin/students" },
-  { key: "users", label: "View Users-DETAILS", path: "/admin/users" },
-  {key:"logout", label:<LogOut />, path:"/signup"},
-];
+import { confirmAction } from "../../shared/components/ConfirmAction";
 
 function AdminLayout() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const confirmed = await confirmAction({
+      title: "Logout?",
+      text: "Are you sure you want to logout?",
+      successTitle: "Logged Out!",
+      successText: "You have been logged out successfully.",
+    });
+
+    if (!confirmed) return;
+
+   
+
+    navigate("/", { replace: true });
+  };
+
+  const NAV_ITEMS = [
+    { key: "dashboard", label: "Dashboard", path: "/admin" },
+    { key: "view", label: "View Scholarships-Available", path: "/admin/view" },
+    { key: "manage", label: "Create-Scholarships", path: "/admin/manage" },
+    { key: "students", label: "View Students-DATA", path: "/admin/students" },
+    { key: "users", label: "View Users-DETAILS", path: "/admin/users" },
+    {
+      key: "logout",
+      label: <LogOut />,
+      onClick: handleLogout,
+    },
+  ];
+
   return (
     <Sidebar title="Admin Panel" navItems={NAV_ITEMS}>
       <Outlet />
