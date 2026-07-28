0364fe39e272ba939358aeff2edcc2993d37e57d
Vighnesh <vghnshpjr@gmail.com>
2026-07-28 13:55:04 +0530

    UPDATE ScholarshipResult
    UPDATE changed displaying of inactive scholarships

--- frontend/src/features/user/pages/ScholarshipResult.jsx
+++ frontend/src/features/user/pages/ScholarshipResult.jsx
@@ -1,6 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import "../components/Scholarship.css";
 import logo from  "../../../assets/new2.ico";
+import {confirmAction} from "../../shared/components/ConfirmAction"
+
 function ScholarshipResult() {
   const location = useLocation();
   const navigate = useNavigate();
@@ -11,19 +13,26 @@ function ScholarshipResult() {
     scholarships = location.state.scholarships;
   }
 
-  const role = localStorage.getItem("role");
+  
+  const handleDirect = async (e, link) => {
+  e.preventDefault();
 
+  const confirmed = await confirmAction({
+    title: "Alert",
+    text: "You will be redirected to another website.",
+    
+  });
+
+  if (confirmed) {
+    window.open(link, "_blank", "noopener,noreferrer");
+  }
+};
+const activeScholarships=scholarships.filter(item=>item.is_active);
+const inactiveScholarships=scholarships.filter(item=> !item.is_active);
   return (
     <div className="scholarship-page">
       <img src={logo} alt="Scholarship Portal Logo" className="logo" />
       <h1>Eligible Scholarships</h1>
-
-      {/* Admin Button */}
-      {role === "admin" && (
-        <button className="admin-btn" onClick={() => navigate("/admin")}>
-          Admin Panel
-        </button>
-      )}
 
       <h3>{scholarships.length} Scholarships Found</h3>
 
@@ -41,70 +50,84 @@ function ScholarshipResult() {
             </strong>
           </h2>
         </div>
-      ) : (
-        <div className="scholarships-grid">
-          {scholarships.map((item, index) => (
-            <div className="scholarship-card" key={index}>
-              <h2>{item.sclrname}</h2>
+      
+         ) : (  
+               
+<>
+  <div className="scholarships-grid">
+    {activeScholarships.map((item) => (
+      <div className="scholarship-card" key={item.sclrid}>
+        <h2>{item.sclrname}</h2>
 
-              <div className="details">
-                <div className="row">
-                  <span className="label">Amount</span>
-                  <span className="value">₹{item.amount}</span>
-                </div>
+        <div className="details">
+          <div className="row">
+            <span className="label">Amount</span>
+            <span className="value">₹{item.amount}</span>
+          </div>
 
-                <div className="row">
-                  <span className="label">Required Percentage</span>
-                  <span className="value">{item.percentreeq}%</span>
-                </div>
+          <div className="row">
+            <span className="label">Required Percentage</span>
+            <span className="value">{item.percentreeq}%</span>
+          </div>
 
-                <div className="row">
-                  <span className="label">Income Limit</span>
-                  <span className="value">₹{item.miniincome}</span>
-                </div>
+          <div className="row">
+            <span className="label">Income Limit</span>
+            <span className="value">₹{item.miniincome}</span>
+          </div>
 
-                <div className="row">
-                  <span className="label">Deadline</span>
-                  <span className="value">{item.deadline}</span>
-                </div>
+          <div className="row">
+            <span className="label">Deadline</span>
+            <span className="value">{item.deadline}</span>
+          </div>
 
-                <div className="row">
-                  <span className="label">Status</span>
-                  <span className="value">
-                    {item.is_active ? "🟢 Available" : "🔴 Closed"}
-                  </span>
-                </div>
+          <div className="row">
+            <span className="label">Status</span>
+            <span className="value">🟢 Available</span>
+          </div>
 
-                <div className="row">
-                  <span className="label">Days Left</span>
-                  <span className="value">
-                    {item.is_active ? item.days_left : "Better luck Next time"}
-                  </span>
-                </div>
-              </div>
-
-              <p className="details-text">Application Link:</p>
-
-              {item.is_active ? (
-                <a
-                  href={item.application_link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="apply-btn"
-                >
-                  Apply Now
-                </a>
-              ) : (
-                <button className="closed-btn" disabled>
-                  Applications Closed
-                </button>
-              )}
-            </div>
-          ))}
+          <div className="row">
+            <span className="label">Days Left</span>
+            <span className="value">{item.days_left}</span>
+          </div>
         </div>
-      )}
+
+        <p className="details-text">Application Link:</p>
+
+        <a
+          href={item.application_link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="apply-btn"
+          onClick={(e) => handleDirect(e, item.application_link)}
+        >
+          Apply Now
+        </a>
+      </div>
+    ))}
+  </div>
+
+  {inactiveScholarships.length > 0 && (
+    <div className="Unavailable-Sclr">
+      <h2>Applications Closed</h2>
+
+      <ol>
+        {inactiveScholarships.map((item) => (
+          <ul key={item.sclrid}>
+            <strong>{item.sclrname}</strong> (Deadline: {item.deadline})
+          </ul>
+        ))}
+      </ol>
     </div>
-  );
-}
+  )}
+</>
+
+         
+         
+         ) }     
+    </div>
+  
+    
+  
+   ); }
 
 export default ScholarshipResult;
