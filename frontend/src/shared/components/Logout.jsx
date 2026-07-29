1ada1e51069a6aaf87914cf3513303f3eb36b078
Vighnesh <vghnshpjr@gmail.com>
2026-07-29 13:40:18 +0530

    UpdateLogout

--- frontend/src/shared/components/Logout.jsx
+++ frontend/src/shared/components/Logout.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { confirmAction } from "../../shared/components/ConfirmAction";
+import {ConfirmAction} from "./ConfirmAction";
 
 function Logout() {
   const navigate = useNavigate();
