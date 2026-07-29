82fced6c24c5800679037fc17cd437f86bbc1a2c
Vighnesh <vghnshpjr@gmail.com>
2026-07-29 13:39:52 +0530

    Update ManagaeSchlr

--- frontend/src/features/admin/pages/ManageScholarships.jsx
+++ frontend/src/features/admin/pages/ManageScholarships.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "../components/ManageScholarships.css";
 import { toast } from "react-toastify";
-import { confirmAction } from "../../shared/components/ConfirmAction";
+import {ConfirmAction} from "../../../shared/components/ConfirmAction";
 function ManageScholarships() {
   const location = useLocation();
   const navigate = useNavigate();
