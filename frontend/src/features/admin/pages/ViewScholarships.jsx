9f92762c2cd4900b42727de241fb12c63cb6d80b
Vighnesh <vghnshpjr@gmail.com>
2026-07-30 11:49:07 +0530

    Update

--- frontend/src/features/admin/pages/ViewScholarships.jsx
+++ frontend/src/features/admin/pages/ViewScholarships.jsx
@@ -140,14 +140,14 @@ function ViewScholarships() {
                 <td>{renderValue(item.caste)}</td>
                 <td>{renderValue(item.educationqualifiation)}</td>
                 <td>{renderValue(item.deadline)}</td>
-
-                <td>
+  <td style={{padding:"20px"}}>
                   {renderValue(item.draft) == 0 ? (
                     <CircleCheck color="#58d57d" />
                   ) : (
                     <SquarePen color="#c41c35" />
                   )}
                 </td>
+              
 
                 <td>
                   <button
