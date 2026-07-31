244c40f071f754b2e423147e60b7e837acc0950b
Vighnesh <vghnshpjr@gmail.com>
2026-07-31 16:30:20 +0530

    Update

--- frontend/src/features/admin/components/AdminLayout.jsx
+++ frontend/src/features/admin/components/AdminLayout.jsx
@@ -21,10 +21,10 @@ function AdminLayout() {
 
   const NAV_ITEMS = [
     { key: "dashboard", label: "Dashboard", path: "/admin" },
-    { key: "view", label: "View Scholarships-Available", path: "/admin/view" },
+    { key: "view", label: "Scholarships", path: "/admin/view" },
     { key: "manage", label: "Create-Scholarships", path: "/admin/manage" },
-    { key: "students", label: "View Students-DATA", path: "/admin/students" },
-    { key: "users", label: "View Users-DETAILS", path: "/admin/users" },
+    { key: "students", label: "Students-Details", path: "/admin/students" },
+    { key: "users", label: "Users-Detais", path: "/admin/users" },
     { key: "logout", label: <LogOut />, path: "/logout" },
   ];
 
