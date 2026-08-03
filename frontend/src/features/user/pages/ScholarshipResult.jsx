c7ff7ecef46523b378d8b22be40b03d085c867aa
Vighnesh <vghnshpjr@gmail.com>
2026-08-03 14:19:04 +0530

    Update

--- frontend/src/features/user/pages/ScholarshipResult.jsx
+++ frontend/src/features/user/pages/ScholarshipResult.jsx
@@ -1,7 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import "../components/Scholarship.css";
-import logo from  "../../../assets/new2.ico";
-import {confirmAction} from "../../../shared/components/ConfirmAction"
+import logo from "../../../assets/new2.ico";
+import { confirmAction } from "../../../shared/components/ConfirmAction";
 
 function ScholarshipResult() {
   const location = useLocation();
@@ -12,23 +12,23 @@ function ScholarshipResult() {
   if (location.state) {
     scholarships = location.state.scholarships;
   }
+  console.log(scholarships);
 
-  
   const handleDirect = async (e, link) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const confirmed = await confirmAction({
-    title: "Alert",
-    text: "You will be redirected to another website.",
-    
-  });
+    const confirmed = await confirmAction({
+      title: "Alert",
+      text: "You will be redirected to another website.",
+    });
 
-  if (confirmed) {
-    window.open(link, "_blank", "noopener,noreferrer");
-  }
-};
-const activeScholarships=scholarships.filter(item=>item.is_active);
-const inactiveScholarships=scholarships.filter(item=> !item.is_active);
+    if (confirmed) {
+      window.open(link, "_blank", "noopener,noreferrer");
+    }
+  };
+
+  const activeScholarships = scholarships.filter(item => item.days_left > 0 && item.draft === 0);
+  const inactiveScholarships = scholarships.filter(item => item.days_left <= 0 || item.draft !== 0);
   return (
     <div className="scholarship-page">
       <img src={logo} alt="Scholarship Portal Logo" className="logo" />
@@ -50,84 +50,77 @@ const inactiveScholarships=scholarships.filter(item=> !item.is_active);
             </strong>
           </h2>
         </div>
-      
-         ) : (  
-               
-<>
-  <div className="scholarships-grid">
-    {activeScholarships.map((item) => (
-      <div className="scholarship-card" key={item.sclrid}>
-        <h2>{item.sclrname}</h2>
+      ) : (
+        <>
+          <div className="scholarships-grid">
+            {activeScholarships.map((item) => (
+              <div className="scholarship-card" key={item.sclrid}>
+                <h2>{item.sclrname}</h2>
 
-        <div className="details">
-          <div className="row">
-            <span className="label">Amount</span>
-            <span className="value">₹{item.amount}</span>
+                <div className="details">
+                  <div className="row">
+                    <span className="label">Amount</span>
+                    <span className="value">₹{item.amount}</span>
+                  </div>
+
+                  <div className="row">
+                    <span className="label">Required Percentage</span>
+                    <span className="value">{item.percentreeq}%</span>
+                  </div>
+
+                  <div className="row">
+                    <span className="label">Income Limit</span>
+                    <span className="value">₹{item.miniincome}</span>
+                  </div>
+
+                  <div className="row">
+                    <span className="label">Deadline</span>
+                    <span className="value">{item.deadline}</span>
+                  </div>
+
+                  <div className="row">
+                    <span className="label">Status</span>
+                    <span className="value">🟢 Available</span>
+                  </div>
+
+                  <div className="row">
+                    <span className="label">Days Left</span>
+                    <span className="value">{item.days_left}</span>
+                  </div>
+                </div>
+
+                <p className="details-text">Application Link:</p>
+
+                <a
+                  href={item.application_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="apply-btn"
+                  onClick={(e) => handleDirect(e, item.application_link)}
+                >
+                  Apply Now
+                </a>
+              </div>
+            ))}
           </div>
 
-          <div className="row">
-            <span className="label">Required Percentage</span>
-            <span className="value">{item.percentreeq}%</span>
-          </div>
+          {inactiveScholarships.length > 0 && (
+            <div className="Unavailable-Sclr">
+              <h2>Applications Closed</h2>
 
-          <div className="row">
-            <span className="label">Income Limit</span>
-            <span className="value">₹{item.miniincome}</span>
-          </div>
-
-          <div className="row">
-            <span className="label">Deadline</span>
-            <span className="value">{item.deadline}</span>
-          </div>
-
-          <div className="row">
-            <span className="label">Status</span>
-            <span className="value">🟢 Available</span>
-          </div>
-
-          <div className="row">
-            <span className="label">Days Left</span>
-            <span className="value">{item.days_left}</span>
-          </div>
-        </div>
-
-        <p className="details-text">Application Link:</p>
-
-        <a
-          href={item.application_link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="apply-btn"
-          onClick={(e) => handleDirect(e, item.application_link)}
-        >
-          Apply Now
-        </a>
-      </div>
-    ))}
-  </div>
-
-  {inactiveScholarships.length > 0 && (
-    <div className="Unavailable-Sclr">
-      <h2>Applications Closed</h2>
-
-      <ol>
-        {inactiveScholarships.map((item) => (
-          <ul key={item.sclrid}>
-            <strong>{item.sclrname}</strong> (Deadline: {item.deadline})
-          </ul>
-        ))}
-      </ol>
+              <ol>
+                {inactiveScholarships.map((item) => (
+                  <ul key={item.sclrid}>
+                    <strong>{item.sclrname}</strong> (Deadline: {item.deadline})
+                  </ul>
+                ))}
+              </ol>
+            </div>
+          )}
+        </>
+      )}
     </div>
-  )}
-</>
-
-         
-         
-         ) }     
-    </div>
-  
-    
-  
-   ); }
+  );
+}
 
 export default ScholarshipResult;
