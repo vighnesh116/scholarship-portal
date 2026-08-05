28daab31454f83965c8e5f745937e8b859f8a9a7
Vighnesh <vghnshpjr@gmail.com>
2026-08-05 18:00:49 +0530

    Update

--- frontend/src/features/admin/components/Pagination.jsx
+++ frontend/src/features/admin/components/Pagination.jsx
@@ -1,18 +1,22 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-function Pagination({ currentPage, totalPages, onPageChange, dataPerPage,pageStart,pageEnd ,totalRecords}) {
+function Pagination({
+  currentPage,
+  totalPages,
+  onPageChange,
+  dataPerPage,
+  pageStart,
+  pageEnd,
+  totalRecords,
+}) {
   if (totalPages <= 1) return null;
 
   return (
     <div className="pagination">
-
-    
-
       <div className="page-info1">
         {pageStart} - {pageEnd} of {totalRecords}
       </div>
-    <> </>
-      
+      <> </>
 
       <div className="page-control">
         <button
@@ -22,7 +26,9 @@ function Pagination({ currentPage, totalPages, onPageChange, dataPerPage,pageSta
           <ChevronLeft />
         </button>
 
-        <div className="page-info1">{currentPage} from {totalPages}</div>
+        <div className="page-info1">
+          {currentPage} from {totalPages}
+        </div>
 
         <button
           disabled={currentPage === totalPages}
