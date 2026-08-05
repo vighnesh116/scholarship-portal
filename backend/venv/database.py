c147dedfffc2d27633183cea184a045d50c25af8
Vighnesh <vghnshpjr@gmail.com>
2026-08-05 13:58:39 +0530

    Update

--- backend/venv/database.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="scholarship_portal"
-    )
