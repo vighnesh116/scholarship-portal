5b3ed123a545ab51abb50787c81e2ebd72691ac7
Vighnesh <vghnshpjr@gmail.com>
2026-08-06 12:29:29 +0530

    Implementing JWT

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
-from pathlib import Path
-import sys
+# from pathlib import Path
+# import sys
+
+
+# BASE_DIR = Path(__file__).resolve().parent
+# if str(BASE_DIR) not in sys.path:
+#     sys.path.insert(0, str(BASE_DIR))
 
 from flask import Flask
 from flask_cors import CORS
-
-BASE_DIR = Path(__file__).resolve().parent
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
+from flask_jwt_extended import JWTManager, create_access_token,jwt_required
 
 from auth import signup, login, update_password
 from scholarship import (
@@ -21,7 +23,8 @@ from student import portal, admin_students, user_data
 
 app = Flask(__name__)
 CORS(app)
-app.config["SECRET_KEY"] = "Nqn4muAADg3RbA"
+app.config["JWT_SECRET_KEY"] = "Nqn4muAADg3RbAgeucbuwbcueich"
+jwt=JWTManager(app)
 
 # Authentication
 app.add_url_rule("/signup", view_func=signup, methods=["POST"])
@@ -38,7 +41,7 @@ app.add_url_rule("/delete-scholarship/<int:id>", view_func=delete_scholarship, m
 
 # Student Management
 app.add_url_rule("/admin-students", view_func=admin_students, methods=["GET"])
-app.add_url_rule("/portal", view_func=portal, methods=["POST"])
+app.add_url_rule("/portal", view_func=jwt_required()(portal), methods=["POST"])
 app.add_url_rule("/admin-users", view_func=user_data, methods=["GET"])
 
 if __name__ == "__main__":
