8e88058c09f964a35108d895a8f7ca577a8accb6
Vighnesh <vghnshpjr@gmail.com>
2026-08-07 18:04:34 +0530

    Update

--- frontend/src/shared/api/axiosInstance.js
+++ frontend/src/shared/api/axiosInstance.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
-const api=axios.create({baseURL:" http://127.0.0.1:5000",});
-api.interceptors.request.use(
-    (config)=>{
-    const token =localStorage.getItem("access_token");
+const api = axios.create({
+  baseURL: "http://127.0.0.1:5000",
+});
 
-    if(token){
-        config.headers.Authorization=`Bearer ${token}`;
+// Add access token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
-},
-(error)=>{
+  },
+  (error) => {
     return Promise.reject(error);
-}
+  }
 );
-export default api ;
+
+export default api;
