909b3e5263fda9fdd73f2e3e392730974122c65f
Vighnesh <vghnshpjr@gmail.com>
2026-08-11 11:14:16 +0530

    UPDATE

--- frontend/src/shared/pages/ErrorPage.jsx
+++ frontend/src/shared/pages/ErrorPage.jsx
@@ -1,29 +1,47 @@
 
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,navigate, replace } from "react-router-dom";
 import "./ErrorPage.css";
 
 function ErrorPage() {
   const navigate = useNavigate();
+  const token = localStorage.getItem("access_token");
+  const role = localStorage.getItem("role");
 
-  return (
-    <div className="error-page">
-      <div className="error-content">
-        <h1>404</h1>
+  if(token && role=="admin"){
+    navigate("/admin",replace=true);
+  }
+  else if(token && role=="student"){
+    navigate("/portal",replace=true);
+  }
+  else if(!token && role){
+    navigate("/",replace=true);
 
-        <h2>Page Not Found</h2>
-
-        <p>
-          Sorry, the page you are looking for doesn't exist
-          or may have been moved.
-        </p>
-
-        <button onClick={() => navigate("/")}>
-          Go Back Home
-        </button>
-      </div>
-    </div>
-  );
+  }
+  else{
+    navigate("/",replace=true);
+  }
 }
 
 export default ErrorPage;
+
+
+
+
+
+ // <div className="error-page">
+    //   <div className="error-content">
+    //     <h1>404</h1>
+
+    //     <h2>Page Not Found</h2>
+
+    //     <p>
+    //       Sorry, the page you are looking for doesn't exist
+    //       or may have been moved.
+    //     </p>
+
+    //     <button onClick={() => navigate("/")}>
+    //       Go Back Home
+    //     </button>
+    //   </div>
+    // </div>
