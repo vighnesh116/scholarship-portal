e7e7a6506bae71ecaf2bfc03b56f2d99d16b2a22
Vighnesh <vghnshpjr@gmail.com>
2026-08-17 12:12:45 +0530

    reduce APP.jsx

--- frontend/src/app/AppRoutes.jsx
+++ frontend/src/app/AppRoutes.jsx
@@ -1,18 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../features/user/pages/Login";
-import Signup from "../features/user/pages/Signup";
-import Portal from "../features/user/pages/Portal";
-import ScholarshipResult from "../features/user/pages/ScholarshipResult";
-import PasswordUpdate from "../features/user/pages/PasswordUpdate";
-import AdminLayout from "../features/admin/components/AdminLayout";
-import AdminDashboard from "../features/admin/pages/AdminDashboard";
-import StudentsDetails from "../features/admin/pages/StudentsDetails";
-import UsersDetails from "../features/admin/pages/UsersDetails";
-import ProtectedRoute from "./ProtectedRoutes";
-import ViewScholarships from "../features/Scholarship/pages/ViewScholarships";
-import Logout from "../shared/components/Logout";
-import CreateScholarship from "../features/Scholarship/pages/CreateScholarship";
-import ErrorPage from "../shared/pages/ErrorPage";
+import  {
+Login ,Signup,Portal,ScholarshipResult ,PasswordUpdate,AdminLayout, AdminDashboard ,StudentsDetails,UsersDetails,ProtectedRoute,ViewScholarships,Logout,CreateScholarship ,ErrorPage} from "./ImportPage"
 function AppRoutes() {
   return (
     <Routes>
