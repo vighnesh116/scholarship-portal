0726e3b34c122050a74270fc006bfc461946672b
Vighnesh <vghnshpjr@gmail.com>
2026-08-28 15:53:32 +0530

    Update Admin Pass

--- frontend/src/features/user/pages/PasswordUpdate.jsx
+++ frontend/src/features/user/pages/PasswordUpdate.jsx
@@ -13,7 +13,7 @@ function PasswordUpdate() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  
+
   const submit = async (e) => {
     e.preventDefault();
 
